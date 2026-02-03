1b29b9b8d1a318d3da2f017b57e2cb0269fc4665
Tibbitazz <54623943+Tibbitazz@users.noreply.github.com>
2026-02-03 17:50:21 +0100

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/boblefri-reimagined-space/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
